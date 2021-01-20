10c82058fe9d3eed1a3b3e7983310d27412c883b
Camilo <camilo@correo.com>
2021-01-19 19:44:47 -0500

    clientes comentados

--- cliente2.py
+++ cliente2.py
@@ -1,9 +1,12 @@
 from socket import *
 
 class Cliente:
+    '''Inicializa el cliente con el nombre y el socket para hacer las conexiones'''
     def __init__(self, nombre):
         self.nombre=nombre
         self.c=socket(AF_INET,SOCK_STREAM)
+    '''Metodo para conectarse a la licorera, recibir los mensajes de ella y
+    y recibir del teclado las opciones del usuario'''
     def conectar_licorera(self):
         self.c.connect(("127.0.0.1",1234))
         self.c.send(self.nombre)
@@ -30,10 +33,12 @@ class Cliente:
                 self.consultar_usuarios(opcion)
                 print opciones
         self.c.close()
+    '''Metodo para pedirle a la licorera que muestre el catalogo con los licores'''
     def consultar_licores(self,opcion):
         self.c.send(opcion)
         data=self.c.recv(1024)
         print data
+    '''Metodo para realizar la compra de un solo licor'''
     def comprar_licor(self,opcion):
         self.c.send(opcion)
         data=self.c.recv(1024)
@@ -42,10 +47,12 @@ class Cliente:
         self.c.send(opcion)
         ans=self.c.recv(1024)
         print ans
+    '''Metodo para realizar la consulta a la licorera de los usuarios conectados'''
     def consultar_usuarios(self,opcion):
         self.c.send(opcion)
         data=self.c.recv(1024)
         print data
 
+'''Inicializacion del codigo'''
 camilo=Cliente("Alexandra")
 camilo.conectar_licorera()
