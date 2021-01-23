2d2160cc48ae68034a69c6c01af9fa8b3458f2a5
Camilo <camilo@correo.com>
2021-01-23 12:09:45 -0500

    doble tcp y udp al tiempo

--- banco.py
+++ banco.py
@@ -13,31 +13,31 @@ class Banco:
     def start(self):
         self.tcpserver.bind((self.ip,self.puerto))
         self.udpserver.bind((self.ip,5050))
-        self.tcpserver.listen(5)
+        #
         print "Esperando conexion..."
+        thread=Thread(target=self.tcp_handler)
+        thread.start()
+        thread2=Thread(target=self.udp_handler)
+        thread2.start()
+    def tcp_handler(self):
+        print "entra al tcp"
         while True:
+            self.tcpserver.listen(5)
             conn, add=self.tcpserver.accept()
             print "Conexion desde ",add
-            thread=Thread(self.tcp_handler, args=(conn,add))
-            thread.start()
-    def tcp_handler(self,conn,add):
-        conn.send("Bienvenido a su banco, a continuacion se mostrara las opciones\n para que ingrese el numero correspondiente")
-        self.opciones(conn)
+            conn.send("Bienvenido a su banco, a continuacion se mostrara las opciones\n para que ingrese el numero correspondiente")
+            self.opciones(conn)
+        #while True:
+    def udp_handler(self):
+        print "Entra al udp"
+        while True:
+            data,remote_host = self.udpserver.recvfrom(1024)
+            print data
+            print remote_host
     def opciones(self,conn):
         menu="1) Consultar saldo\n"+"2) Retirar\n"+"3) Consignar\n"+"4) Salir\n"
         conn.send(menu)
 
 
-server=socket(AF_INET,SOCK_DGRAM)
-server.bind(("127.0.0.1",5050))
-print "Esperando conexion..."
-while True:
-    datos, host_remoto=server.recvfrom(1024)
-    print datos
-    if datos=="bye\n":
-        server.close()
-        break
-    elif datos=="PAGAR":
-        server.sendto("ACEPTADO",host_remoto)
-    else:
-        server.sendto("Hola desde el banco\n",host_remoto)
+banco=Banco("127.0.0.1",6789)
+banco.start()
