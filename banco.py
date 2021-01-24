284ecb190071b36762a56bf2aace1639d99fa951
Camilo <camilo@correo.com>
2021-01-24 17:52:59 -0500

    banco y cliente tcp

--- banco.py
+++ banco.py
@@ -1,5 +1,6 @@
 from socket import *
 from threading import *
+import os
 
 class Banco:
     def __init__(self, ip,puerto):
@@ -13,15 +14,15 @@ class Banco:
         self.tcpserver=socket(AF_INET,SOCK_STREAM)
         self.udpserver=socket(AF_INET,SOCK_DGRAM)
     def start(self):
-        self.tcpserver.bind((self.ip,self.puerto))
-        self.udpserver.bind((self.ip,5050))
-        #
+        try:
+            self.tcpserver.bind((self.ip,self.puerto))
+            #self.tcpserver.listen(5)
+        except:
+            print "puerto ocupado o INBOX equivocado"
+            self.tcpserver.close()
+            os._exit(0)       #
         print "Esperando conexion..."
-        #thread2=Thread(target=self.udp_handler)
-        #thread2.start()
         while True:
-            thread2=Thread(target=self.udp_handler)
-            thread2.start()
             self.tcpserver.listen(5)
             conn,add=self.tcpserver.accept()#Acepta la conexion de un usuario
             print "Conexion desde ",add
@@ -29,10 +30,15 @@ class Banco:
             thread.start()
 
     def tcp_handler(self,conn,add):
+        #self.tcpserver.listen(5)
+        #conn,add=self.tcpserver.accept()#Acepta la conexion de un usuario
+        #print "Conexion desde ",add
         bienvenida= "Bienvenido a su Banco, para nosotros es un gusto atenderlo\n"
         conn.send(bienvenida)
         self.autenticacion (conn,add)
         conn.send("\t\tLas opciones para interactuar con nosotros son las siguientes\n")
+        ok=conn.recv(50)
+        print ok
         self.opciones(conn)
         while True:
             datos=conn.recv(1024)
@@ -47,10 +53,18 @@ class Banco:
                 self.consignar(conn,add)
             else:
                 conn.send("Lo sentimos, la opcion no esta disponible")
+        print "Cerrando conexion"
         conn.close()
 
     def udp_handler(self):
         print "Entra al udp"
+        try:
+            self.udpserver.bind((self.ip,5050))
+        except:
+            print "puerto ocupado o INBOX equivocado"
+            self.udpserver.close()
+            os._exit(0)
+        #print "Entra al udp"
         while True:
             data,remote_host = self.udpserver.recvfrom(1024)
             print data
@@ -60,16 +74,23 @@ class Banco:
 
     def autenticacion(self,conn,add):
         conn.send("Ingrese Usuario")
-        usuario=conn.recv(1024)
+        usuario=str(conn.recv(1024))
+        print usuario
         if usuario in self.db["Nombres"]:
+            print "entro al if"
             self.index= self.db["Nombres"].index(usuario)
             dir_user.append(add)
             user_list.append(usuario)
-            conn.send("Usuario Valido")
-            conn.send("Ingrese la contrasena")
+            conn.send("Usuario Valido\n")
+            ok=conn.recv(30)
+            print ok
+            conn.send("Ingrese la contrasena\n")
             contrasena= conn.recv(1024)
+            print contrasena
             if contrasena == self.db["Contrasenas"][self.index]:
                 conn.send("Ingreso Exitoso")
+                ok=conn.recv(40)
+                print ok
             else:
                 conn.send("Contrasena no Valida...cerrando conexion")
                 conn.close()   
@@ -80,7 +101,7 @@ class Banco:
 
 
     def opciones(self,conn):#Muestra al usuario las opciones disponibles para interactuar con el Banco
-        menu="1)SALDO: para conocer su saldo disponible \n"+"2)RETIRAR: para retirar dinero de su cuenta\n"+"3)CONSIGNAR: para retirar dinero de su cuenta\n"+"4)SALIR: para desconectarse del banco\n"
+        menu="1)SALDO: para conocer su saldo disponible\n"+"2)RETIRAR: para retirar dinero de su cuenta\n"+"3)CONSIGNAR: para retirar dinero de su cuenta\n"+"4)SALIR: para desconectarse del banco\n"
         conn.send(menu)   
     
     def saldo(self,conn,add):
@@ -118,5 +139,6 @@ class Banco:
 user_list=[]
 dir_user=[]
 banco=Banco("127.0.0.1",6789)
+thread1=Thread(target=banco.udp_handler,args=())
+thread1.start()
 banco.start()
-     
--- cliente.py
+++ cliente.py
@@ -4,10 +4,11 @@ class Cliente:
     '''Inicializa el cliente con el nombre y el socket para hacer las conexiones'''
     def __init__(self, nombre):
         self.nombre=nombre
-        self.c=socket(AF_INET,SOCK_STREAM)
+        #self.c=socket(AF_INET,SOCK_STREAM)
     '''Metodo para conectarse a la licorera, recibir los mensajes de ella y
     y recibir del teclado las opciones del usuario'''
     def conectar_licorera(self):
+        self.c=socket(AF_INET,SOCK_STREAM)
         self.c.connect(("127.0.0.1",1234))
         self.c.send(self.nombre)
         bienvenida=self.c.recv(1024)
@@ -50,7 +51,92 @@ class Cliente:
         self.c.send(opcion)
         data=self.c.recv(1024)
         print data
+    def conectar_banco(self):
+        self.c=socket(AF_INET,SOCK_STREAM)
+        self.c.connect(("127.0.0.1",6789))
+        bienvenida=self.c.recv(1024)
+        print bienvenida
+        segundo=self.c.recv(1024)
+        print segundo
+        user=input()
+        self.c.send(str(user))
+        res1=self.c.recv(1024)
+        self.c.send("OK")
+        print "res1"
+        print res1
+        print "--------------------"
+        if res1=="Usuario Valido\n":
+            tercero=self.c.recv(1024)
+            print tercero
+            pasw=input("Contrasena: ")
+            self.c.send(str(pasw))
+            conf=self.c.recv(52)
+            if conf=="Ingreso Exitoso":
+                self.c.send("OK")
+                print "if passw"
+                print conf
+                conectado=True
+                print conectado
+                opciones=self.c.recv(1024)
+                print opciones
+                self.c.send("OK")
+                menu=self.c.recv(1024)
+                print menu
+            else:
+                print conf
+                conectado=False
+        else: 
+            ans=self.c.recv(1024)
+            print ans
+            conectado=False
+
+        
+        while conectado:
+            opcion=str(input("Escriba una opcion: "))
+            if opcion=="SALDO":
+                self.consultar_saldo(opcion)
+                print menu
+            elif opcion=="SALIR":
+                self.c.send(opcion)
+                break
+            elif opcion=="CONSIGNAR":
+                self.consignar(opcion)
+                print menu
+            elif opcion=="RETIRAR":
+                self.retirar(opcion)
+                print menu
+        print "Saliendo"
+        self.c.close()
+    def consultar_saldo(self,opcion):
+        self.c.send(opcion)
+        ans=self.c.recv(1024)
+        print ans
+    def consignar(self,opcion):
+        self.c.send(opcion)
+        ans=self.c.recv(1024)
+        print ans
+        vlr=input("Valor: ")
+        self.c.send(vlr)
+        ans2=self.c.recv(1024)
+        print ans2
+    def retirar(self,opcion):
+        self.c.send(opcion)
+        ans=self.c.recv(1024)
+        print ans
+        vlr=input("Monto: ")
+        self.c.send(vlr)
+        ans2=self.c.recv(1024)
+        print ans2
 
 '''Inicializacion del codigo'''
 camilo=Cliente("Camilo")
-camilo.conectar_licorera()
+print "Hola, las opciones son las siguientes: "
+while True:
+    print "1) Conectar con la licorera"
+    print "2) Conectar con el banco"
+    opcion=int(input("Ingrese el numero de la opcion deseada: "))
+    if opcion==1:
+        camilo.conectar_licorera()
+    elif opcion==2:
+        camilo.conectar_banco()
+        
